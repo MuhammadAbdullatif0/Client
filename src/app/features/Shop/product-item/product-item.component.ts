52fee5a8f3e3a0e1cd1c369dc9108b73d3b39d01
Muhammad Abdulltif <pingcairodevelopers@gmail.com>
2024-10-17 10:15:06 +0300

    Routing

--- src/app/features/Shop/product-item/product-item.component.ts
+++ src/app/features/Shop/product-item/product-item.component.ts
@@ -8,17 +8,19 @@ import {
 import { MatIcon } from '@angular/material/icon';
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
   imports: [
     MatCard,
-    MatIcon,
     MatCardContent,
     MatCardActions,
+    MatIcon,
     CurrencyPipe,
     MatButton,
+    RouterLink,
   ],
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css'],
